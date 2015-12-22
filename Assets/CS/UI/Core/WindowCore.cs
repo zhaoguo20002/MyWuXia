ab394bdc324d8ace236817c9fce6fd53a8d0deff
apple <zhaoguo2004@126.com>
2015-12-22 11:48:21 +0800

    修复UI框架维护资源集合的bug

--- Assets/CS/UI/Core/WindowCore.cs
+++ Assets/CS/UI/Core/WindowCore.cs
@@ -106,17 +106,11 @@ namespace Game {
 		}
 
 		/// <summary>
-		/// The wins.
-		/// </summary>
-		static Dictionary<string, GameObject> windows = null;
-
-		static Dictionary<string, T> _ctrls = null;
-		/// <summary>
 		/// The ctrls.
 		/// </summary>
 		protected static Dictionary<string, T> Ctrls {
 			get {
-				return _ctrls;
+				return UIModel<T>.Ctrls;
 			}
 		}
 
@@ -138,24 +132,28 @@ namespace Game {
 		/// <param name="offsetWidth">Offset width.</param>
 		/// <param name="offsetHeight">Offset height.</param>
 		protected static void InstantiateView(string path, string id = "", float offsetWidth = 0, float offsetHeight = 0) {
-			if (windows == null) {
-				windows = new Dictionary<string, GameObject>();
-				_ctrls = new Dictionary<string, T>();
+			if (UIModel.Windows == null) {
+				UIModel.Windows = new Dictionary<string, GameObject>();
 			}
+
+			if (UIModel<T>.Ctrls == null) {
+				UIModel<T>.Ctrls = new Dictionary<string, T>();
+			}
+
 			if (id == "") {
 				id = typeof(T).ToString();	
 			}
-			if (!windows.ContainsKey(id)) {
+			if (!UIModel.Windows.ContainsKey(id)) {
 				GameObject winObj = CreateUIPrefab(UIModel.UICanvas.transform, path, offsetWidth, offsetHeight);
 				if (winObj != null) {
 					winObj.name = id;
-					windows.Add(id, winObj);
+					UIModel.Windows.Add(id, winObj);
 					_ctrl = winObj.GetComponent<T>();
-					_ctrls.Add(id, _ctrl);
+					UIModel<T>.Ctrls.Add(id, _ctrl);
 					IWindowInterface iWindowInterface = (IWindowInterface)_ctrl;
 					iWindowInterface.SetId(id);
 				}
-				Debug.LogWarning("InstantiateView - " + windows.Count + "," + _ctrls.Count + "," + (_ctrl));
+				Debug.LogWarning("InstantiateView - " + UIModel.Windows.Count + "," + UIModel<T>.Ctrls.Count + "," + (_ctrl));
 			}
 		}
 
@@ -166,15 +164,15 @@ namespace Game {
 		private int timer = 1;//计时器
 
 		protected static void DestroyView(string id) {
-			if (windows != null && windows.ContainsKey(id)) {
-				Destroy(windows[id]);
-				windows.Remove(id);
-				_ctrls.Remove(id);
+			if (UIModel.Windows != null && UIModel.Windows.ContainsKey(id)) {
+				Destroy(UIModel.Windows[id]);
+				UIModel.Windows.Remove(id);
+				UIModel<T>.Ctrls.Remove(id);
 				if (_ctrl != null && ((IWindowInterface)_ctrl).GetId() == id) {
 					_ctrl = default(T);
 				}
 			}
-			Debug.LogWarning("DestroyView - " + windows.Count + "," + _ctrls.Count + "," + (_ctrl));
+			Debug.LogWarning("DestroyView - " + UIModel.Windows.Count + "," + UIModel<T>.Ctrls.Count + "," + (_ctrl));
 		}
 
 		/// <summary>

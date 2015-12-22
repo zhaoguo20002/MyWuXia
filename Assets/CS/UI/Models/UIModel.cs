c44fa64a4554f78af6ba32cc384737e2b2e1b1a8
apple <zhaoguo2004@126.com>
2015-12-22 11:50:09 +0800

    添加UI数据模型目录

---
+++ Assets/CS/UI/Models/UIModel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Game {
+	public class UIModel {
+		/// <summary>
+		/// The font canvas.
+		/// </summary>
+		public static GameObject FontCanvas = null;
+		/// <summary>
+		/// The frame canvas.
+		/// </summary>
+		public static GameObject FrameCanvas = null;
+		/// <summary>
+		/// The user interface canvas.
+		/// </summary>
+		public static GameObject UICanvas = null;
+		/// <summary>
+		/// 窗口集合
+		/// </summary>
+		public static Dictionary<string, GameObject> Windows = null;
+	}
+
+	public class UIModel<T> {
+		/// <summary>
+		/// 窗口控制器集合
+		/// </summary>
+		public static Dictionary<string, T> Ctrls;
+	}
+}

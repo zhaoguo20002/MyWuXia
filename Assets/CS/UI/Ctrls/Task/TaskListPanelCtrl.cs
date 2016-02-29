90197e241675af2b95e1ccb8c85b0361726912ca
silmazing <670771357@qq.com>
2016-02-29 16:44:08 +0800

    新增

---
+++ Assets/CS/UI/Ctrls/Task/TaskListPanelCtrl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using Game;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+public class TaskListPanelCtrl : WindowCore<TaskListPanelCtrl, JArray> {
+	protected override void Init () {
+
+	}
+
+	public override void UpdateData (object obj) {
+
+	}
+
+	public override void RefreshView () {
+
+	}
+
+	public static void Show(List<TaskData> data) {
+		if (Ctrl == null) {
+
+		}
+	}
+}

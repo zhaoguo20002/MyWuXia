7494755ae7a187112e7d43eb778128ea0227b881
赵 果 <suker@zhaomatoMacBook-Pro.local>
2016-06-14 01:23:26 +0800

    新增资源查看器，和资源icon图标

--- Assets/CS/UI/Models/WorkshopModel.cs
+++ Assets/CS/UI/Models/WorkshopModel.cs
@@ -22,15 +22,15 @@ namespace Game {
 				Relationships.Add(new ResourceRelationshipData(ResourceType.Wood, "2", new List<ResourceData>() { new ResourceData(ResourceType.Food, 1) }, 3, 0.7f, 0.4f, 1));
 				Relationships.Add(new ResourceRelationshipData(ResourceType.Iron, "2", new List<ResourceData>() { new ResourceData(ResourceType.Food, 2) }, 1, 4, 2.2f, 3));
 				Relationships.Add(new ResourceRelationshipData(ResourceType.SilverOre, "1", new List<ResourceData>() { new ResourceData(ResourceType.Food, 10) }, 1, 20, 11, 12));
-				Relationships.Add(new ResourceRelationshipData(ResourceType.Steel, "3", new List<ResourceData>() { new ResourceData(ResourceType.Iron, 4) }, 1, 16, 8.8f, 15));
 				Relationships.Add(new ResourceRelationshipData(ResourceType.Silver, "1", new List<ResourceData>() { new ResourceData(ResourceType.SilverOre, 1) }, 11, 1.8f, 1, -1));
+				Relationships.Add(new ResourceRelationshipData(ResourceType.Steel, "3", new List<ResourceData>() { new ResourceData(ResourceType.Iron, 4) }, 1, 16, 8.8f, 15));
 				Relationships.Add(new ResourceRelationshipData(ResourceType.Tungsten, "3", new List<ResourceData>() { new ResourceData(ResourceType.Silver, 4) }, 1, 7.3f, 4, 6));
 				Relationships.Add(new ResourceRelationshipData(ResourceType.Jade, "3", new List<ResourceData>() { new ResourceData(ResourceType.Silver, 5) }, 1, 9.1f, 5, -1));
-				Relationships.Add(new ResourceRelationshipData(ResourceType.RedSteel, "3", new List<ResourceData>() { new ResourceData(ResourceType.Tungsten, 3), new ResourceData(ResourceType.Stone, 1) }, 1, 22.5f, 12.4f, 20));
 				Relationships.Add(new ResourceRelationshipData(ResourceType.RefinedSteel, "3", new List<ResourceData>() { new ResourceData(ResourceType.Steel, 10) }, 1, 160, 88, 150));
+				Relationships.Add(new ResourceRelationshipData(ResourceType.RedSteel, "3", new List<ResourceData>() { new ResourceData(ResourceType.Tungsten, 3), new ResourceData(ResourceType.Stone, 1) }, 1, 22.5f, 12.4f, 20));
+				Relationships.Add(new ResourceRelationshipData(ResourceType.DarksteelIngot, "3", new List<ResourceData>() { new ResourceData(ResourceType.Silver, 310) }, 1, 6200, 3410, 4500));
 				Relationships.Add(new ResourceRelationshipData(ResourceType.TungstenSteel, "3", new List<ResourceData>() { new ResourceData(ResourceType.Tungsten, 100) }, 1, 727.3f, 400, 600));
 				Relationships.Add(new ResourceRelationshipData(ResourceType.Zingana, "3", new List<ResourceData>() { new ResourceData(ResourceType.Wood, 900), new ResourceData(ResourceType.TungstenSteel, 1) }, 1, 1327.3f, 730, 1500));
-				Relationships.Add(new ResourceRelationshipData(ResourceType.DarksteelIngot, "3", new List<ResourceData>() { new ResourceData(ResourceType.Silver, 310) }, 1, 6200, 3410, 4500));
 			}
 		}
 	}
